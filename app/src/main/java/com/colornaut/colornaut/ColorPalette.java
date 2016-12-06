5cfc85c10e16f8f79a3c025decbb0b9b6403075e
Victor De Souza <vdesouza@me.com>
2016-12-06 10:39:48 -0500

    work on the Palette object

--- app/src/main/java/com/colornaut/colornaut/ColorPalette.java
+++ app/src/main/java/com/colornaut/colornaut/ColorPalette.java
@@ -3,6 +3,9 @@ package com.colornaut.colornaut;
 import android.graphics.Bitmap;
 import android.support.v7.graphics.Palette;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by vdesouza on 12/5/16.
@@ -14,20 +17,42 @@ public class ColorPalette {
 
     // Swatches that make up a color palette
     // A Swatch is one type of color that appears the most
-    // There are a max of 7 default colors but custom targets can be made http://stackoverflow.com/a/28776080
     // Swatches also carry various information about the color picked
     // https://developer.android.com/reference/android/support/v7/graphics/Palette.Swatch.html
 
     private Palette mPalette;
+    private Bitmap image;
+    private List<Palette.Swatch> paletteSwatches;
+    private ArrayList<Integer> rgbValues = new ArrayList<Integer>();
+    private ArrayList<String> hexValues = new ArrayList<String>();
+    private String paletteName;
+    private String location;
+
 
     public ColorPalette(Bitmap image) {
+        this.image = image;
         Palette.Builder paletteBuilder = Palette.from(image);
         paletteBuilder.generate(new Palette.PaletteAsyncListener() {
             public void onGenerated(Palette palette) {
                 mPalette = palette;
             }
         });
+        this.paletteSwatches = mPalette.getSwatches();
+        for (int i = 0; i <= paletteSwatches.size(); i++) {
+            this.rgbValues.add(i, paletteSwatches.get(i).getRgb());
+        }
     }
+
+    public List<Palette.Swatch> getPaletteSwatches() { return paletteSwatches; }
+    public ArrayList<Integer> getRgbValues() { return rgbValues; }
+    public Integer getRgbValue(int position) { return rgbValues.get(position); }
+    public ArrayList<String> getHexValuess() { return hexValues; }
+    public String getHexValue(int position) { return hexValues.get(position); }
+    public String getPaletteName() { return paletteName; }
+    public String getLocation() { return location; }
+    public Bitmap getImage() { return image; }
+
+
 
 
 
