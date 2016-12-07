8019d959251852abba52b47d2d7cad513e5a4221
Victor De Souza <vdesouza@me.com>
2016-12-06 22:14:34 -0500

    Saving loading functions

--- app/src/main/java/com/colornaut/colornaut/ColorPalette.java
+++ app/src/main/java/com/colornaut/colornaut/ColorPalette.java
@@ -1,9 +1,12 @@
 package com.colornaut.colornaut;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.graphics.Palette;
+import android.util.Base64;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +43,17 @@ public class ColorPalette {
         }
     }
 
+    public ColorPalette(String name, String location, List<Palette.Swatch> swatchList) {
+        this.paletteName = name;
+        //this.image = stringToBitmap(bitmapString);
+        this.location = location;
+        this.paletteSwatches = swatchList;
+        for (int i = 0; i <= paletteSwatches.size() - 1; i++) {
+            this.rgbValues.add(i, paletteSwatches.get(i).getRgb());
+            Log.i(TAG,i + " : " + rgbValues.get(i).toString());
+        }
+    }
+
     public List<Palette.Swatch> getPaletteSwatches() { return paletteSwatches; }
     public ArrayList<Integer> getSwatchesRgb() {
         ArrayList<Integer> list = new ArrayList<>();
@@ -55,6 +69,44 @@ public class ColorPalette {
     public String getPaletteName() { return paletteName; }
     public String getLocation() { return location; }
     public Bitmap getImage() { return image; }
+
+
+    @Override
+    public String toString() {
+        String s = "";
+
+        if (paletteName == null) {
+            s = s + "Color Palette" + "\n";
+        } else {
+            s = s + this.paletteName + "\n";
+        }
+
+        //s = s + bitmapToString(this.image) + "\n";
+        if (location == null) {
+            s = s + "no location" + "\n";
+        } else {
+            s = s + this.location + "\n";
+        }
+        s = s + "swatches:\n";
+        for (Palette.Swatch ps : this.paletteSwatches) {
+            s = s + ps.getRgb() + "\n";
+            Log.i(TAG, "SAVING population: " + ps.getPopulation());
+            s = s + ps.getPopulation() + "\n";
+        }
+        s = s + "\n";
+        return s;
+    }
+
+
+    public final static String bitmapToString(Bitmap in){
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        in.compress(Bitmap.CompressFormat.PNG, 100, bytes);
+        return Base64.encodeToString(bytes.toByteArray(),Base64.DEFAULT);
+    }
+    public final static Bitmap stringToBitmap(String in){
+        byte[] bytes = Base64.decode(in, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
 
 
 
--- app/src/main/java/com/colornaut/colornaut/MainActivity.java
+++ app/src/main/java/com/colornaut/colornaut/MainActivity.java
@@ -1,10 +1,27 @@
 package com.colornaut.colornaut;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -17,7 +34,10 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.test.LoaderTestCase;
 import android.text.InputType;
+import android.util.ArraySet;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -35,12 +55,16 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "COLORNAUT";
+//    private static final String FILE_NAME = "ColornautData.txt";
+    private static final String PREFS_KEY = "ColornautData";
+    private SharedPreferences prefs;
 
     // views for main screen
     private Camera mCamera;
@@ -55,6 +79,10 @@ public class MainActivity extends AppCompatActivity {
 
     // views for edit panel
     private ViewGroup editPanel;
+    private LinearLayout editPanelLinearLayout;
+    private EditText inputPaletteName;
+    private Button saveButton;
+    private ColorPreviewsGridAdapter mAdapter;
     private boolean isPanelShown;
 
     @Override
@@ -63,6 +91,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         mContext = this;
+
+        prefs = mContext.getSharedPreferences("pref", Context.MODE_PRIVATE);
 
         // get camera if available
         mCamera = getCameraInstance();
@@ -77,6 +107,8 @@ public class MainActivity extends AppCompatActivity {
         editPanel = (ViewGroup) findViewById(R.id.edit_panel);
         editPanel.setVisibility(View.INVISIBLE);
         isPanelShown = false;
+
+        editPanelLinearLayout = (LinearLayout) findViewById(R.id.editPanelLinearLayout);
 
         // set up capture button
         mCaptureButton = (Button) findViewById(R.id.button_capture);
@@ -143,31 +175,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void slideDown(View v) {
-        closeEditPanel();
+    private void launchEditPanel() {
+        Log.i(TAG, "Launching edit");
+        Animation bottomUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_up);
+        editPanel.startAnimation(bottomUp);
+        editPanel.setVisibility(View.VISIBLE);
+        editPanel.bringToFront();
+        Log.i(TAG, "Launched edit");
+        isPanelShown = true;
+
+        editPanelLinearLayout.removeView(inputPaletteName);
+        editPanelLinearLayout.removeView(saveButton);
+
+        // build gridview of palette colors
+        GridView mPaletteGridView = (GridView) findViewById(R.id.paletteGridView);
+        mAdapter = new ColorPreviewsGridAdapter(this, colorPalette.getSwatchesRgb());
+        mPaletteGridView.setAdapter(mAdapter);
+
+        // Set an EditText view to get user input and Save button
+        inputPaletteName = new EditText(mContext);
+        inputPaletteName.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        inputPaletteName.setHint("Enter palette name");
+        editPanelLinearLayout.addView(inputPaletteName);
+
+        saveButton = new Button(mContext);
+        saveButton.setText("Save");
+        saveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePalette(colorPalette);
+            }
+        });
+        editPanelLinearLayout.addView(saveButton);
+
     }
 
-    private void launchEditPanel() {
-      //  if (!isPanelShown) {
-            Log.i(TAG, "Launching edit");
-            Animation bottomUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_up);
-            editPanel.startAnimation(bottomUp);
-            editPanel.setVisibility(View.VISIBLE);
-            editPanel.bringToFront();
-            Log.i(TAG, "Launched edit");
-            isPanelShown = true;
-
-            // build gridview of palette colors
-            GridView mPaletteGridView = (GridView) findViewById(R.id.paletteGridView);
-            mPaletteGridView.setAdapter(new ColorPreviewsGridAdapter(this, colorPalette.getSwatchesRgb()));
-
-//            // Set an EditText view to get user input
-//            final EditText inputPaletteName = new EditText(mContext);
-//            inputPaletteName.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
-//            inputPaletteName.setHint("Enter palette name");
-//            RelativeLayout editPanelRelativeLayout = (RelativeLayout) findViewById(R.id.editPanelRelativeLayout);
-//            editPanelRelativeLayout.addView(inputPaletteName);
-        //}
+    public void slideDown(View v) {
+        closeEditPanel();
     }
 
     private void closeEditPanel() {
@@ -177,9 +221,135 @@ public class MainActivity extends AppCompatActivity {
             editPanel.startAnimation(bottomDown);
             editPanel.setVisibility(View.INVISIBLE);
             Log.i(TAG, "Closed edit");
+            editPanelLinearLayout.removeView(inputPaletteName);
+            editPanelLinearLayout.removeView(saveButton);
             isPanelShown = false;
         }
     }
 
+    private void savePalette(ColorPalette colorPalette) {
+
+        Set<String> paletteSet = prefs.getStringSet(PREFS_KEY, new HashSet<String>());
+        SharedPreferences.Editor editor = prefs.edit();
+        paletteSet.add(colorPalette.toString());
+        editor.putStringSet(PREFS_KEY, paletteSet);
+        editor.apply();
+
+        Toast.makeText(mContext, "Palette Saved!", Toast.LENGTH_SHORT).show();
+
+        ArrayList<ColorPalette> loadedlist = load();
+        Log.i(TAG, "Load complete");
+        for (ColorPalette cp : loadedlist) {
+            Log.i(TAG, cp.toString());
+        }
+
+
+
+//        PrintWriter writer = null;
+//        try {
+//            FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+//            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+//                    fos)));
+//            Toast.makeText(mContext, "Saving...", Toast.LENGTH_LONG).show();
+//            writer.println(colorPalette);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (null != writer) {
+//                writer.close();
+//                Toast.makeText(mContext, "Palette Saved!", Toast.LENGTH_SHORT).show();
+//                ArrayList<ColorPalette> loadedlist = load();
+//                Log.i(TAG, "Load complete");
+//                for (ColorPalette cp : loadedlist) {
+//
+//                    Log.i(TAG, cp.toString());
+//                }
+//            }
+//        }
+
+        closeEditPanel();
+    }
+
+    private ArrayList<ColorPalette> load() {
+        ArrayList<ColorPalette> listOfSavedPalettes = new ArrayList<ColorPalette>();
+
+        BufferedReader reader = null;
+        try {
+
+            Set<String> paletteSet = prefs.getStringSet(PREFS_KEY, new HashSet<String>());
+
+            for (String s : paletteSet) {
+                reader = new BufferedReader(new StringReader(s));
+
+                Log.i(TAG, "String: " + s);
+
+                String name;
+                //String bitmap;
+                String location;
+                String color;
+                String population;
+                String empty = "";
+
+                while (null != (name = reader.readLine())) {
+                    //bitmap = reader.readLine();
+                    location = reader.readLine();
+                    Log.i(TAG, "Loading: Name = "+name);
+                    Log.i(TAG, "Loading: Location = "+location);
+                    List<Palette.Swatch> swatchList = new ArrayList<Palette.Swatch>();
+                    while (!empty.equals(reader.readLine())) {
+                        color = reader.readLine();
+                        Log.i(TAG, "Loading: Color = "+color);
+                        population = reader.readLine();
+                        Log.i(TAG, "Loading: Population = "+population);
+                        Palette.Swatch ps = new Palette.Swatch(Integer.valueOf(color), Integer.valueOf(population));
+                        swatchList.add(ps);
+                    }
+                    ColorPalette cp = new ColorPalette(name, location, swatchList);
+                    listOfSavedPalettes.add(cp);
+                }
+
+            }
+//            FileInputStream fis = openFileInput(FILE_NAME);
+//            reader = new BufferedReader(new InputStreamReader(fis));
+//
+//            String name;
+//            String bitmap;
+//            String location;
+//            String color;
+//            String population;
+//            String empty = "";
+//
+//            while (null != (name = reader.readLine())) {
+//                bitmap = reader.readLine();
+//                location = reader.readLine();
+//                Log.i(TAG, "Loading: Name = "+name);
+//                Log.i(TAG, "Loading: Location = "+location);
+//                List<Palette.Swatch> swatchList = new ArrayList<Palette.Swatch>();
+//                while (!empty.equals(reader.readLine())) {
+//                    color = reader.readLine();
+//                    Log.i(TAG, "Loading: Color = "+color);
+//                    population = reader.readLine();
+//                    Log.i(TAG, "Loading: Population = "+population);
+//                    Palette.Swatch ps = new Palette.Swatch(Integer.valueOf(color), Integer.valueOf(population));
+//                    swatchList.add(ps);
+//                }
+//                ColorPalette cp = new ColorPalette(name, bitmap, location, swatchList);
+//                listOfSavedPalettes.add(cp);
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != reader) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return listOfSavedPalettes;
+        }
+
+    }
 
 }
